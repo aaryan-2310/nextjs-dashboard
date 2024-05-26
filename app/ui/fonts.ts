3b468f0402fd9f21ead18ae9753a778d027fd8b2
aaryan-2310 <61946336+aaryan-2310@users.noreply.github.com>
2024-05-26 17:25:52 +0530

    changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana, Raleway } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+export const raleway = Raleway({ subsets: ['latin']});
